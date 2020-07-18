aefc7b6f67fd3c0605d67746ff928dd9e48e0edf
Aakash <saakashsundar007@gmail.com>
2020-07-18 12:44:08 +0530

    modified:   navigation/BottomTabNavigator.tsx 	modified:   screens/TabTwoScreen.tsx

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -22,14 +22,14 @@ export default function BottomTabNavigator() {
         name="Click Here Aruna"
         component={TabOneNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color="rgba(0,255,255,1)" />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-body" color="rgba(0,255,255,1)" />,
         }}
       />
       <BottomTab.Screen
         name="Click Here Dheena"
         component={TabTwoNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color="rgba(0,255,255,1)"
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-body" color="rgba(0,255,255,1)"
  />,
         }}
       />
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from '../components/Themed'; 
 
 export default function TabTwoScreen() {
   return (
