96116610fcfba27828595ca5d28b9cf19d32f455
Aakash <saakashsundar007@gmail.com>
2020-07-18 12:01:24 +0530

    modified:   constants/Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,14 +4,14 @@ const tintColorDark = '#fff';
 export default {
   light: {
     text: '#000',
-    background: '#rgba(0,255,0,1)',
+    background: '#rgba(200,200,200,1)',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: "rgba(0,255,255,1)",
-    background: 'rgba(255,0,0,0.3)',
+    background: 'rgba(20,20,20,0.3)',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
